11a35020254236acbce9270ceaa26cc03fac3341
Thomas Gläßle <t_glaessle@gmx.de>
2018-08-21 18:44:18 +0200

    Use wildcard certificates

--- cert-config.sh
+++ cert-config.sh
@@ -1,6 +1,5 @@
 email=t_glaessle@gmx.de
 domains=(
     {coldfix,fireflake}.{de,eu}
-    {www,lists,pypi,gogs,stats,kansha,cryptpad,pins,blog,sudoku,pics,pix,picbox,piwik,goa,goaccess}.{coldfix,fireflake}.{de,eu}
-    {smtp,imap,pop,pop3,ntop,status,mail}.{coldfix,fireflake}.{de,eu}
+    *.{coldfix,fireflake}.{de,eu}
 )
--- cert-renew.sh
+++ cert-renew.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker pull certbot/certbot > /dev/null
+#docker pull certbot/dns-netcup > /dev/null
 
 root=$(readlink -f $(dirname "$BASH_SOURCE"))
 source "$root/cert-config.sh"
@@ -13,17 +13,20 @@ fi
 # NOTE: for renewal only, we could simply run `renew --renew-hook HOOK`
 # NOTE: to force renewal, replace '--keep-until-expiring' with '--renew-by-default'
 docker run --rm \
-    -v "$root/var/acme":/var/www \
     -v "$root/var/letsencrypt":/var/lib/letsencrypt \
     -v /etc/letsencrypt:/etc/letsencrypt \
     --cap-drop=all \
-    certbot/certbot certonly --no-self-upgrade \
-        --keep-until-expiring --non-interactive \
+    certbot/dns-netcup certonly \
+        --authenticator certbot-dns-netcup:dns-netcup \
+        --certbot-dns-netcup:dns-netcup-credentials /var/lib/letsencrypt/netcup_credentials.ini \
+        --certbot-dns-netcup:dns-netcup-propagation-seconds 900 \
+        --no-self-upgrade \
+        --keep-until-expiring --non-interactive --expand \
+        --server https://acme-v02.api.letsencrypt.org/directory \
         --email "$email" --text --agree-tos \
-        --webroot -w /var/www  \
-        --renew-hook 'touch /var/www/.updated' \
+        --renew-hook 'touch /var/lib/letsencrypt/.updated' \
         ${domains[@]/#/-d } "$@"
 
-if rm "$root/var/acme/.updated" 2>/dev/null; then
+if rm "$root/var/letsencrypt/.updated" 2>/dev/null; then
     exec "$root/cert-reload.sh"
 fi
